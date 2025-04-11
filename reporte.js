cee03095aa51dbb51f9500e1202d30b3efc4fff6
Msq03 <71024685+Msq03@users.noreply.github.com>
2025-04-11 01:03:01 -0500

    desafio culminado

--- reporte.js
+++ reporte.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 
-const archivo = process.argv[2]; // Ej: node reporte.js data.csv
+const archivo = process.argv[2]; // Para correr el proyecto solo lo llamariamos así: node reporte.js data.csv
 
-if (!archivo) {
+if (!archivo) { //Si no especificamos el archivo que se leerá, me mostrará el mensaje.
   console.error("Por favor, especifica el archivo CSV.");
   process.exit(1);
 }
 
-const ruta = path.resolve(__dirname, archivo);
+const ruta = path.resolve(__dirname, archivo); // con __dirname Obtenemos la ruta actual del archivo a leer, el cual esta en la misma ruta de la clase reporte.js
 fs.readFile(ruta, 'utf8', (err, data) => {
   if (err) {
     console.error("Error leyendo el archivo:", err.message);
@@ -16,6 +16,7 @@ fs.readFile(ruta, 'utf8', (err, data) => {
   }
 
   const lineas = data.trim().split('\n').slice(1); // Ignora encabezado
+  //Asigno los valores iniciales de mis variables
   let balance = 0;
   let mayorMonto = 0;
   let idMayor = null;
@@ -23,23 +24,23 @@ fs.readFile(ruta, 'utf8', (err, data) => {
   let debitos = 0;
 
   for (const linea of lineas) {
-    const [id, tipo, montoStr] = linea.split(',');
+    const [id, tipo, montoStr] = linea.split(','); 
     const monto = parseFloat(montoStr);
 
-    if (tipo === 'Crédito') {
+    if (tipo === 'Crédito') { //Sumo todas las transacciones con tipo Crédito
       balance += monto;
       creditos++;
-    } else if (tipo === 'Débito') {
+    } else if (tipo === 'Débito') {  //Sumo todas las transacciones con tipo Débito
       balance -= monto;
       debitos++;
     }
 
-    if (monto > mayorMonto) {
+    if (monto > mayorMonto) { //Busco el monto mayor por cada transacción
       mayorMonto = monto;
       idMayor = id;
     }
   }
-
+//VISTA EN LA TERMINAL
   console.log("Reporte de Transacciones");
   console.log("---------------------------------------------");
   console.log(`Balance Final: ${balance.toFixed(2)}`);
